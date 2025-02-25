50950937993546f7db1c43af08129c4ebfcca23e
github-action-benchmark <github@users.noreply.github.com>
2025-02-25 03:10:39 +0000

    add Marko Benchmarks (customSmallerIsBetter) benchmark result for bf175944fdc514b763e7d0c493ab9d2bc4515da7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740452894707,
+  "lastUpdate": 1740453039362,
   "repoUrl": "https://github.com/DylanPiercey/benchmark-ci-test",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,50 @@ window.BENCHMARK_DATA = {
             "value": 5539.72142650463,
             "unit": "ns/iter",
             "range": "(5415.960205078125 ns … 5799.622802734375 ns)"
+          }
+        ]
+      }
+    ],
+    "Marko Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "dpiercey@ebay.com",
+            "name": "dpiercey",
+            "username": "DylanPiercey"
+          },
+          "committer": {
+            "email": "dpiercey@ebay.com",
+            "name": "dpiercey",
+            "username": "DylanPiercey"
+          },
+          "distinct": true,
+          "id": "bf175944fdc514b763e7d0c493ab9d2bc4515da7",
+          "message": "chore: setup test project",
+          "timestamp": "2025-02-24T20:10:10-07:00",
+          "tree_id": "eb15267d0b43ba8c76b70e61fa229682e3b82036",
+          "url": "https://github.com/DylanPiercey/benchmark-ci-test/commit/bf175944fdc514b763e7d0c493ab9d2bc4515da7"
+        },
+        "date": 1740453038989,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "deleting 1 from object",
+            "value": 47.67179455153879,
+            "unit": "ns/iter",
+            "range": "(43.296142578125 ns … 77.464111328125 ns)"
+          },
+          {
+            "name": "deleting 10 from object",
+            "value": 407.54617178154905,
+            "unit": "ns/iter",
+            "range": "(398.611572265625 ns … 719.18408203125 ns)"
+          },
+          {
+            "name": "deleting 100 from object",
+            "value": 5408.029994419643,
+            "unit": "ns/iter",
+            "range": "(5332.749755859375 ns … 5556.4541015625 ns)"
           }
         ]
       }
