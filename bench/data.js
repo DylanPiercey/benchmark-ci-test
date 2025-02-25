cfc327977af95cb369d52727014815c173fd2ec7
github-action-benchmark <github@users.noreply.github.com>
2025-02-25 03:14:37 +0000

    add Marko Benchmarks (customSmallerIsBetter) benchmark result for 665c5ce1855fd922a181bd64396d530f0991f605

---
+++ bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740453277217,
+  "repoUrl": "https://github.com/DylanPiercey/benchmark-ci-test",
+  "entries": {
+    "Marko Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "dpiercey@ebay.com",
+            "name": "dpiercey",
+            "username": "DylanPiercey"
+          },
+          "committer": {
+            "email": "dpiercey@ebay.com",
+            "name": "dpiercey",
+            "username": "DylanPiercey"
+          },
+          "distinct": true,
+          "id": "665c5ce1855fd922a181bd64396d530f0991f605",
+          "message": "chore: setup test project",
+          "timestamp": "2025-02-24T20:14:10-07:00",
+          "tree_id": "dc242fce5fd97be88febf376d0e01bac00f94ba0",
+          "url": "https://github.com/DylanPiercey/benchmark-ci-test/commit/665c5ce1855fd922a181bd64396d530f0991f605"
+        },
+        "date": 1740453276993,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "deleting 1 from object",
+            "value": 46.80205652652167,
+            "unit": "ns/iter",
+            "range": "(43.046875 ns … 100.40747070324142 ns)"
+          },
+          {
+            "name": "deleting 10 from object",
+            "value": 406.3460783673926,
+            "unit": "ns/iter",
+            "range": "(396.652587890625 ns … 708.850341796875 ns)"
+          },
+          {
+            "name": "deleting 100 from object",
+            "value": 5398.331708635603,
+            "unit": "ns/iter",
+            "range": "(5320.7734375 ns … 5596.831787109375 ns)"
+          },
+          {
+            "name": "deleting 1000 from object",
+            "value": 54156.47528076174,
+            "unit": "ns/iter",
+            "range": "(53113.76904296875 ns … 55771.15966796875 ns)"
+          }
+        ]
+      }
+    ]
+  }
+}
