78e5936b368d65f63f99e29b00510b4e7b6af8e3
github-action-benchmark <github@users.noreply.github.com>
2025-02-25 03:08:14 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for dc69e59e8e15f6504f2443759fef6c9b6587857a

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740452894707,
+  "repoUrl": "https://github.com/DylanPiercey/benchmark-ci-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dpiercey@ebay.com",
+            "name": "dpiercey",
+            "username": "DylanPiercey"
+          },
+          "committer": {
+            "email": "dpiercey@ebay.com",
+            "name": "dpiercey",
+            "username": "DylanPiercey"
+          },
+          "distinct": true,
+          "id": "dc69e59e8e15f6504f2443759fef6c9b6587857a",
+          "message": "chore: setup test project",
+          "timestamp": "2025-02-24T20:07:41-07:00",
+          "tree_id": "dbeb34ed51a1b0252d784e6065035b0f37c537a1",
+          "url": "https://github.com/DylanPiercey/benchmark-ci-test/commit/dc69e59e8e15f6504f2443759fef6c9b6587857a"
+        },
+        "date": 1740452893834,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "deleting 1 from object",
+            "value": 46.627898127371445,
+            "unit": "ns/iter",
+            "range": "(44.6240234375 ns … 96.553955078125 ns)"
+          },
+          {
+            "name": "deleting 10 from object",
+            "value": 425.6160302734375,
+            "unit": "ns/iter",
+            "range": "(404.49951171875 ns … 709.749755859375 ns)"
+          },
+          {
+            "name": "deleting 100 from object",
+            "value": 5539.72142650463,
+            "unit": "ns/iter",
+            "range": "(5415.960205078125 ns … 5799.622802734375 ns)"
+          }
+        ]
+      }
+    ]
+  }
+}
