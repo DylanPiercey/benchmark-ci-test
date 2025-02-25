665c5ce1855fd922a181bd64396d530f0991f605
dpiercey <dpiercey@ebay.com>
2025-02-24 19:54:50 -0700

    chore: setup test project

---
+++ bench.ts
@@ -0,0 +1,45 @@
+import fs from "node:fs";
+import * as b from "mitata";
+
+b.boxplot(() => {
+  b.bench("deleting $keys from object", function* (state) {
+    const keys = state.get("keys");
+
+    const obj = {};
+    for (let i = 0; i < keys; i++) obj[i] = i;
+
+    yield {
+      [0]() {
+        return { ...obj };
+      },
+
+      bench(p0) {
+        for (let i = 0; i < keys; i++) delete p0[i];
+      },
+    };
+  }).args("keys", [1, 10, 100, 1000]);
+});
+
+const { benchmarks } = await b.run();
+const results: {
+  name: string;
+  unit: string;
+  value: number;
+  range: string;
+}[] = [];
+
+for (const { runs } of benchmarks) {
+  for (const run of runs) {
+    if (run.stats) {
+      results.push({
+        name: run.name,
+        unit: "ns/iter",
+        value: run.stats.avg,
+        range: `(${run.stats.min} ns … ${run.stats.max} ns)`,
+      });
+    }
+  }
+}
+
+fs.mkdirSync("dist", { recursive: true });
+fs.writeFileSync("dist/benchmark.json", JSON.stringify(results, null, 2));
